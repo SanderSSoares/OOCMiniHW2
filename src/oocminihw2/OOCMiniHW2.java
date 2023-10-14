ebe9774ff51cbe3f39f8ddc1aa21d0e483e3fe02
Sander2022164 <2022164@student.cct.ie>
2023-10-14 11:28:18 +0100

    Main class
    In the main class the objects were created, and methods used

--- src/oocminihw2/OOCMiniHW2.java
+++ src/oocminihw2/OOCMiniHW2.java
@@ -15,17 +15,30 @@ public class OOCMiniHW2 {
      */
     public static void main(String[] args){
         
+        //Calling each objects and using all methods.
         
+        //Calling the car
         System.out.println("----CREATING A CAR----");
         Car car1 = new Car();
         car1.accelerate(50);
         car1.brake();
         car1.turn(30);
         
+        //Calling the plane
         System.out.println("----CREATING A PLANE----");
+        Plane plane1 = new Plane();
+        plane1.accelerate(285);
+        plane1.brake();
+        plane1.turn(60);
+        plane1.changeAltitude(3000);
+        
+        //Calling the boat
         System.out.println("----CREATING A BOAT----");
-        
-        
+        Boat boat1 = new Boat();
+        boat1.hoistSail();
+        boat1.isSailHoisted();
+        boat1.landHo();
+        boat1.lowerSail();   
     }
     
 }
